5fd40a4a15691fbd0269a7255a92784e101f9dba
JosiahFu <josiahfu@gmail.com>
2023-10-27 14:54:50 -0700

    Revert "Remove unnecessary client code and mixins"
    This reverts commit 26843f095a93782366967dd7cb4ba9cd49190a70.

---
+++ src/client/java/archives/tater/doorjam/DoorJamClient.java
@@ -0,0 +1,10 @@
+package archives.tater.doorjam;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DoorJamClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
