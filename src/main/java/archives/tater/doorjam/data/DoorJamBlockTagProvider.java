710f0e89605749da78c6fb48f29fec11606a1d3c
DP <digitalpeargaming@gmail.com>
2025-01-21 11:25:03 -0800

    added metal fences compatibility

--- src/main/java/archives/tater/doorjam/data/DoorJamBlockTagProvider.java
+++ src/main/java/archives/tater/doorjam/data/DoorJamBlockTagProvider.java
@@ -1,5 +1,6 @@
 package archives.tater.doorjam.data;
 
+import com.google.common.collect.ImmutableMap;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.block.Block;
@@ -8,6 +9,7 @@ import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper.WrapperLookup;
 import net.minecraft.util.Identifier;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 public class DoorJamBlockTagProvider extends FabricTagProvider<Block> {
@@ -16,23 +18,28 @@ public class DoorJamBlockTagProvider extends FabricTagProvider<Block> {
         super(output, RegistryKeys.BLOCK, registriesFuture);
     }
 
+    private static final Map<String, String> COMPAT_LIST = ImmutableMap.of(
+            "shutter", "shutter",
+            "metalfences", "fence_gate"
+    );
+
     @Override
     protected void configure(WrapperLookup wrapperLookup) {
         getOrCreateTagBuilder(DoorJamBlockTags.SLIGHT_JAMMING_CHANCE)
                 .add(Blocks.EXPOSED_COPPER_DOOR)
-                .add(Blocks.EXPOSED_COPPER_TRAPDOOR)
-                .addOptional(Identifier.of("shutter", "exposed_copper_shutter"))
-        ;
+                .add(Blocks.EXPOSED_COPPER_TRAPDOOR);
 
         getOrCreateTagBuilder(DoorJamBlockTags.HALF_JAMMING_CHANCE)
                 .add(Blocks.WEATHERED_COPPER_DOOR)
-                .add(Blocks.WEATHERED_COPPER_TRAPDOOR)
-                .addOptional(Identifier.of("shutter", "weathered_copper_shutter"))
-        ;
+                .add(Blocks.WEATHERED_COPPER_TRAPDOOR);
         getOrCreateTagBuilder(DoorJamBlockTags.FULL_JAMMING_CHANCE)
                 .add(Blocks.OXIDIZED_COPPER_DOOR)
-                .add(Blocks.OXIDIZED_COPPER_TRAPDOOR)
-                .addOptional(Identifier.of("shutter", "oxidized_copper_shutter"))
-        ;
+                .add(Blocks.OXIDIZED_COPPER_TRAPDOOR);
+
+        COMPAT_LIST.forEach((modid, itemName) -> {
+            getOrCreateTagBuilder(DoorJamBlockTags.SLIGHT_JAMMING_CHANCE).addOptional(Identifier.of(modid, "exposed_copper_" + itemName));
+            getOrCreateTagBuilder(DoorJamBlockTags.HALF_JAMMING_CHANCE).addOptional(Identifier.of(modid, "weathered_copper_" + itemName));
+            getOrCreateTagBuilder(DoorJamBlockTags.FULL_JAMMING_CHANCE).addOptional(Identifier.of(modid, "oxidized_copper_" + itemName));
+        });
     }
 }
